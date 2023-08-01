5af31d95b00c9eb73d6713b8f69fa9c1d2e0f71a
AudieDev <audieshacodes@gmail.com>
2023-08-01 11:02:01 -0500

    Adding tailwind css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import './App.css';
-import React from "react";
-import Header from "./Header";
-import { render } from '@testing-library/react';
+import React from 'react';
+import Navbar from './Navbar';
 
-function App() {
-const newList = render();
-return (
-<div>
-  <h1> Here is some text to practice my react</h1>
-<ol>
-  <li> Hey There </li>
-  <li>Have some text</li>
-  <li>With a little react on the side</li>
-</ol>
-</div>
-); 
-// Using the newly created Header
-// component in this main component
-<Header/>
-;
-}
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* Other components and content */}
+    </div>
+  );
+};
+
 export default App;
-
-
